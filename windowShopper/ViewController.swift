0d257692e285494018688288354d71280639afab
Viktor Lutsenko <lutsenko.xcode@gmail.com>
2021-06-09 23:34:44 +0300

    Initial Commit

---
+++ windowShopper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  windowShopper
+//
+//  Created by ViktorLutsenko on 09.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
